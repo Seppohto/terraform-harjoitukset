c1058a521b9cb09646f1de294e502cb66c767698
seppohto <olli.uronen@gmail.com>
2022-03-07 10:43:47 +0200

    eka terroform commit. created RG, Vnet and added variables

---
+++ main.tf
@@ -0,0 +1,36 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  subscription_tenant_id = var.subscription_tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "OllinTFResourceGroup"
+  location = "westeurope"
+  
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name                = "olliTFVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name
+}
+
